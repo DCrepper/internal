c0788fcb204d1730e5efc4c963be247403e5536d
Szabó Zoltán <zoli.szabok@gmail.com>
2024-04-08 12:40:44 +0200

    Update directory path in create_laravel_app.sh

--- create_laravel_app.sh
+++ create_laravel_app.sh
@@ -3,4 +3,4 @@
 github_repo_url="https://github.com/DCrepper/outer.git"
 # Create a new directory for the cloned repository
 # Clone the GitHub repository
-git clone $github_repo_url ../$1
+git clone $github_repo_url ../../$1
