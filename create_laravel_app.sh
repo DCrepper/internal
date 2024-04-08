8a1eab2327c7d6f412f9fa70bf8b5ec97f409cfe
Szabó Zoltán <zoli.szabok@gmail.com>
2024-04-08 12:18:33 +0200

    Update process instantiation in NewAppCreator.php and create_laravel_app.sh

--- create_laravel_app.sh
+++ create_laravel_app.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 # Define the GitHub repository URL
 github_repo_url="https://github.com/DCrepper/outer.git"
-
 # Create a new directory for the cloned repository
-
-outer_root_folder="../$1"
-mkdir -p $outer_root_folder
-
 # Clone the GitHub repository
-git clone $github_repo_url $outer_root_folder
-# Check if the required arguments are provided
-
-
+git clone $github_repo_url ../$1
